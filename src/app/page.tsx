68e5944f660062b005b3c3c82bfc79a77f3759c9
elmersson <elmerssonrasmus@gmail.com>
2024-03-04 18:07:03 +0100

    feat: navbar component (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,9 @@
 import { Boot } from "@/components/boot";
 import { Desktop } from "@/components/desktop";
 import { Login } from "@/components/login";
-import Wallpaper from "@/components/wallpaper";
 import { useSystem } from "@/hooks/useSystem";
 
 export default function Home() {
-  const { booted, logedIn } = useSystem();
-  return (
-    <>
-      {!booted && <Boot />}
-      {logedIn ? <Desktop /> : <Login />}
-      <Wallpaper />
-    </>
-  );
+  const { logedIn } = useSystem();
+  return <>{logedIn ? <Desktop /> : <Login />}</>;
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useSystem } from "@/hooks/useSystem";
+import useTime from "@/hooks/useTime";
+import getFullFormatDate from "@/lib/date/getFullFormatDate";
+import { cn } from "@/lib/utils";
+import { IoLogoApple } from "react-icons/io5";
+
+export function Navbar() {
+  const { logedIn } = useSystem();
+
+  const timeHook = useTime();
+  const { dayOfWeek, dayOfMonth, month, time } = getFullFormatDate(timeHook);
+
+  return (
+    <div
+      className={cn(
+        "flex justify-between py-2 px-2 fixed top-0 left-0 right-0 z-50 w-full",
+        logedIn &&
+          "bg-slate-800 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10 dark:bg-slate-800/40"
+      )}
+    >
+      <div className="flex flex-row items-center">
+        <div className="px-2.5 rounded-md py-1">
+          <IoLogoApple className="text-white size-[18px] drop-shadow-lg" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-1 text-sm text-slate-300">
+        <span>{dayOfWeek.substring(0, 3)}</span>
+        <span>{dayOfMonth}</span>
+        <span>{month.substring(0, 3)}</span>
+        <span>{time}</span>
+      </div>
+    </div>
+  );
+}
