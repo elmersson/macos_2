40af2570999b06d1f505e01a2bd4c64f7c1e7abe
elmersson <elmerssonrasmus@gmail.com>
2024-04-10 17:31:44 +0200

    feat: display brightness (#16)

--- src/components/navbar/control-centre.tsx
+++ src/components/navbar/control-centre.tsx
@@ -14,12 +14,26 @@ import { Display } from "./display";
 import { Sound } from "./sound";
 import { Music } from "./music";
 import { useAudio } from "@/hooks/useAudio";
+import { useSystem } from "@/hooks/useSystem";
+
+function getOpacityClass(displayValue: number) {
+  if (displayValue >= 90) return "opacity-100";
+  if (displayValue >= 75) return "opacity-90";
+  if (displayValue >= 50) return "opacity-75";
+  if (displayValue >= 25) return "opacity-50";
+  if (displayValue > 15) return "opacity-25";
+  if (displayValue > 0) return "opacity-15";
+  return "opacity-0";
+}
 
 export function ControlCentre() {
+  const { display } = useSystem();
   const [_audio, state, controls, _ref] = useAudio({
     src: "/music/Stockholmsvy.mp3",
     autoReplay: true,
   });
+
+  const opacityClass = getOpacityClass(display);
 
   return (
     <DropdownMenu>
@@ -28,6 +42,7 @@ export function ControlCentre() {
           role="button"
           className={"flex items-center px-2.5 py-1 rounded-md"}
         >
+          git
           <Image
             src={ControlCenterImage}
             height={14}
@@ -36,7 +51,9 @@ export function ControlCentre() {
           />
         </div>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-[340px] mt-1 rounded-xl space-y-2 p-3">
+      <DropdownMenuContent
+        className={`w-[340px] mt-1 rounded-xl space-y-2 p-3 ${opacityClass}`}
+      >
         <div className="flex flex-row space-x-2">
           <ConnectControl />
           <div className="flex flex-col space-y-2 w-[50%]">
