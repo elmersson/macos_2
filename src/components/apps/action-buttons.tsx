a9898bbfd25f35732af232474b9d811b5efdf407
elmersson <elmerssonrasmus@gmail.com>
2024-04-13 08:32:55 +0200

    OHD-286 (#19)

---
+++ src/components/apps/action-buttons.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { PiMinusBold } from 'react-icons/pi';
+import { RiExpandLeftRightFill } from 'react-icons/ri';
+import { RxCross2 } from 'react-icons/rx';
+
+interface ActionButtonsProps {
+  exit: () => void;
+  fullSize: () => void;
+}
+
+export default function ActionButtons({ exit, fullSize }: ActionButtonsProps) {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div
+      className="flex items-center"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <div
+        className="rounded-full h-3 w-3 bg-[#FF6157] border-[#E24640] mx-2 flex justify-center items-center"
+        onClick={exit}
+      >
+        {hovered && (
+          <RxCross2 style={{ color: 'text-slate-900', fontSize: '0.55rem' }} />
+        )}
+      </div>
+      <div className="rounded-full h-3 w-3 bg-[#FFC12F] border-[#DFA023] mr-2 flex justify-center items-center">
+        {hovered && (
+          <PiMinusBold
+            style={{ color: 'text-slate-900', fontSize: '0.55rem' }}
+          />
+        )}
+      </div>
+      <div
+        className="rounded-full h-3 w-3 bg-[#2ACB42] border-[#1BAC2C] flex justify-center items-center"
+        onClick={fullSize}
+      >
+        {hovered && (
+          <RiExpandLeftRightFill
+            style={{
+              color: 'text-slate-900',
+              fontSize: '0.55rem',
+              transform: 'rotate(45deg)',
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
--- src/components/desktop.tsx
+++ src/components/desktop.tsx
@@ -1,8 +1,10 @@
+import { VSCode } from "./apps/vscode";
 import Dock from "./dock";
 
 export function Desktop() {
   return (
     <div className="flex overflow-hidden no-scrollbar">
+      <VSCode />
       <Dock />
     </div>
   );
