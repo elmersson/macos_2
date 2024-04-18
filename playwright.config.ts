702ba3ddfa15ea6c971e60f81e5b6b27d75b99a6
RasmusElmersson <rasmus.elmersson@martinservera.se>
2024-04-18 12:54:15 +0200

    fix: workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,20 @@
-import { type PlaywrightTestConfig } from '@playwright/test';
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  fullyParallel: false,
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    }
+  ],
+
   webServer: {
     command: 'npm run dev',
     port: 3000,
