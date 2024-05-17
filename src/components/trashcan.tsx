44ed0878fe09c4fe64a531eb0ca124d3c2db3284
RasmusElmersson <rasmus.elmersson@martinservera.se>
2024-05-17 07:42:44 +0200

    feat trashcan

---
+++ src/components/trashcan.tsx
@@ -0,0 +1,40 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+  TooltipArrow
+} from '@/components/ui/tooltip';
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import Empty from '@/assets/apps/empty-trashcan.png';
+import TrashcanImage from '@/assets/apps/trashcan.png';
+
+export function Trashcan() {
+  const isOpen = false;
+  const isEmpty = true;
+
+  const img = isEmpty ? Empty : TrashcanImage;
+
+  return (
+    <TooltipProvider delayDuration={100}>
+      <Tooltip>
+        <TooltipTrigger>
+          <li className="flex flex-col justify-center items-center relative overflow-hidden no-scrollbar">
+            <Image src={img} alt="trashcan" className=" w-[4.2rem]" />
+            <div
+              className={cn(
+                'h-1 w-1 rounded-full bg-white/60 mt-1',
+                !isOpen && 'opacity-0'
+              )}
+            />
+          </li>
+        </TooltipTrigger>
+        <TooltipContent className="mb-2">
+          Bin
+          <TooltipArrow />
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
