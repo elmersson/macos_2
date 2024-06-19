0847d029968348e2e935743c4ae46f144a0a0217
elmersson <elmerssonrasmus@gmail.com>
2024-06-19 07:13:40 +0200

    feat: app menu (#82)

--- src/components/navbar/app-menu.tsx
+++ src/components/navbar/app-menu.tsx
@@ -11,86 +11,57 @@ import {
   DropdownMenuTrigger
 } from '../ui/dropdown-menu';
 import { useAppStore } from '../providers/store-provider';
+import { Fragment } from 'react';
+import { AppData, MenuItem, Trigger } from '@/data/Apps';
 
 export function AppMenu() {
-  const { activeApp } = useAppStore((state) => state);
+  const { activeApp, apps } = useAppStore((state) => state);
+  const activeAppData = apps.find((app: AppData) => app.id === activeApp);
+
+  if (!activeAppData?.triggers) {
+    return null;
+  }
+
+  const renderMenuItems = (menuItems: MenuItem[]) => {
+    return menuItems.map((item) => (
+      <Fragment key={item.id}>
+        <DropdownMenuItem onSelect={item.action}>
+          <p className="text-s">{item.title}</p>
+          {item.shortcut && (
+            <DropdownMenuShortcut>{item.shortcut}</DropdownMenuShortcut>
+          )}
+        </DropdownMenuItem>
+        {item.subMenu && (
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <p className="text-s">{item.title}</p>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                {renderMenuItems(item.subMenu)}
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+        )}
+        {item.separator && <DropdownMenuSeparator />}
+      </Fragment>
+    ));
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <span role="button" className="text-sm drop-shadow-lg">
-          {activeApp}
-        </span>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="mt-1">
-        <DropdownMenuItem>
-          <p className="text-s">About this Mac</p>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className="flex justify-between">
-          <p className="text-s">System Settings...</p>
-          <div className="items-center px-3 rounded-xl bg-slate-400/50">
-            <p className="text-xs ">1 Update</p>
-          </div>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="flex justify-between">
-          <p className="text-s">App Store...</p>
-          <div className="items-center px-3 rounded-xl bg-slate-400/50">
-            <p className="text-xs ">11 Updates</p>
-          </div>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuSub>
-          <DropdownMenuSubTrigger>
-            <p className="text-s">Recent Items</p>
-          </DropdownMenuSubTrigger>
-          <DropdownMenuPortal>
-            <DropdownMenuSubContent>
-              <p className="text-xs font-bold text-slate-400/50 px-2">
-                Applications
-              </p>
-              <DropdownMenuItem>
-                <p className="text-s">App</p>
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <p className="text-xs font-bold text-slate-400/50 px-2">
-                Documents
-              </p>
-              <DropdownMenuSeparator />
-              <p className="text-xs font-bold text-slate-400/50 px-2">
-                Servers
-              </p>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <p className="text-s">Clear Menu</p>
-              </DropdownMenuItem>
-            </DropdownMenuSubContent>
-          </DropdownMenuPortal>
-        </DropdownMenuSub>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <p className="text-s">Force Quit</p>
-          <DropdownMenuShortcut>⌥⌘⎋</DropdownMenuShortcut>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <p className="text-s">Sleep</p>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <p className="text-s">Restart...</p>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <p className="text-s">Shut Down...</p>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <p className="text-s">Lock Screen</p>
-          <DropdownMenuShortcut>⌃⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <p className="text-s mr-8">Log Out Rasmus Elmersson...</p>
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="space-x-4">
+      {activeAppData.triggers.map((trigger: Trigger) => (
+        <DropdownMenu key={trigger.id}>
+          <DropdownMenuTrigger asChild>
+            <span role="button" className="text-sm drop-shadow-lg">
+              {trigger.label}
+            </span>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mt-1">
+            {renderMenuItems(trigger.menu)}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ))}
+    </div>
   );
 }
