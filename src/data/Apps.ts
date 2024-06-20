e4f4c50ead71f2f230707f3b230ad912c69844cd
RasmusElmersson <rasmus.elmersson@martinservera.se>
2024-06-20 08:17:21 +0200

    feat: finder menu data

--- src/data/Apps.ts
+++ src/data/Apps.ts
@@ -20,7 +20,7 @@ export interface MenuItem {
   id: string;
   title: string;
   shortcut?: string;
-  action: () => void;
+  action?: () => void;
   subMenu?: MenuItem[];
   separator?: boolean;
 }
@@ -65,28 +65,13 @@ export const apps: AppData[] = [
         id: 'title',
         label: 'Finder',
         menu: [
-          {
-            id: 'about',
-            title: 'About Finder',
-            action: () => console.log('About Finder')
-          },
-          {
-            id: 'settings',
-            title: 'Settings...',
-            action: () => console.log('Open Settings')
-          },
-          {
-            id: 'separator1',
-            title: '',
-            action: () => {},
-            separator: true
-          },
-          {
-            id: 'quit',
-            title: 'Quit Finder',
-            shortcut: '⌘Q',
-            action: () => console.log('Quit Finder')
-          }
+          { id: 'about-finder', title: 'About Finder' },
+          { id: 'settings', title: 'Settings...', shortcut: '⌘,' },
+          { id: 'empty-bin', title: 'Empty Bin...', shortcut: '⇧⌘⌫' },
+          { id: 'services', title: 'Services', subMenu: [] },
+          { id: 'hide-finder', title: 'Hide Finder', shortcut: '⌘H' },
+          { id: 'hide-others', title: 'Hide Others', shortcut: '⌥⌘H' },
+          { id: 'show-all', title: 'Show All' }
         ]
       },
       {
@@ -94,46 +79,83 @@ export const apps: AppData[] = [
         label: 'File',
         menu: [
           {
-            id: 'general',
-            title: 'General',
-            action: () => console.log('General Preferences')
+            id: 'new-finder-window',
+            title: 'New Finder Window',
+            shortcut: '⌘N'
           },
+          { id: 'new-folder', title: 'New Folder', shortcut: '⇧⌘N' },
           {
-            id: 'appearance',
-            title: 'Appearance',
-            action: () => console.log('Appearance Preferences')
-          }
+            id: 'new-folder-with-selection',
+            title: 'New Folder with Selection',
+            shortcut: '⌃⌘N'
+          },
+          { id: 'new-smart-folder', title: 'New Smart Folder' },
+          { id: 'new-tab', title: 'New Tab', shortcut: '⌘T' },
+          { id: 'open', title: 'Open', shortcut: '⌘O' },
+          { id: 'close-window', title: 'Close Window', shortcut: '⌘W' },
+          { id: 'get-info', title: 'Get Info', shortcut: '⌘I' },
+          { id: 'rename', title: 'Rename' },
+          { id: 'compress', title: 'Compress' },
+          { id: 'duplicate', title: 'Duplicate', shortcut: '⌘D' },
+          { id: 'make-alias', title: 'Make Alias', shortcut: '⌃⌘A' },
+          { id: 'quick-look', title: 'Quick Look', shortcut: '⌘Y' },
+          { id: 'print', title: 'Print', shortcut: '⌘P' },
+          { id: 'share', title: 'Share...' },
+          {
+            id: 'show-enclosing-folder',
+            title: 'Show in Enclosing Folder',
+            shortcut: '⌘R'
+          },
+          { id: 'add-to-dock', title: 'Add to Dock', shortcut: '⌃⇧⌘T' },
+          { id: 'move-to-bin', title: 'Move to Bin', shortcut: '⌘⌫' },
+          { id: 'eject', title: 'Eject', shortcut: '⌘E' },
+          { id: 'tags', title: 'Tags...' },
+          { id: 'find', title: 'Find', shortcut: '⌘F' }
         ]
       },
       {
         id: 'edit',
         label: 'Edit',
         menu: [
-          {
-            id: 'general',
-            title: 'General',
-            action: () => console.log('General Preferences')
-          },
-          {
-            id: 'appearance',
-            title: 'Appearance',
-            action: () => console.log('Appearance Preferences')
-          }
+          { id: 'undo', title: 'Undo', shortcut: '⌘Z' },
+          { id: 'redo', title: 'Redo', shortcut: '⇧⌘Z' },
+          { id: 'cut', title: 'Cut', shortcut: '⌘X' },
+          { id: 'copy', title: 'Copy', shortcut: '⌘C' },
+          { id: 'paste', title: 'Paste', shortcut: '⌘V' },
+          { id: 'select-all', title: 'Select All', shortcut: '⌘A' },
+          { id: 'show-clipboard', title: 'Show Clipboard' },
+          { id: 'autofill', title: 'AutoFill', subMenu: [] },
+          { id: 'start-dictation', title: 'Start Dictation...' },
+          { id: 'emoji-symbols', title: 'Emoji & Symbols' }
         ]
       },
       {
         id: 'view',
         label: 'View',
         menu: [
+          { id: 'as-icons', title: 'as Icons', shortcut: '⌘1' },
+          { id: 'as-list', title: 'as List', shortcut: '⌘2' },
+          { id: 'as-columns', title: 'as Columns', shortcut: '⌘3' },
+          { id: 'as-gallery', title: 'as Gallery', shortcut: '⌘4' },
+          { id: 'use-groups', title: 'Use Groups', shortcut: '⌃⌘0' },
+          { id: 'sort-by', title: 'Sort By', subMenu: [] },
+          { id: 'show-tab-bar', title: 'Show Tab Bar', shortcut: '⇧⌘T' },
+          { id: 'show-all-tabs', title: 'Show All Tabs', shortcut: "⇧⌘'" },
+          { id: 'hide-sidebar', title: 'Hide Sidebar', shortcut: '⌥⌘S' },
+          { id: 'show-preview', title: 'Show Preview', shortcut: '⇧⌘P' },
+          { id: 'hide-toolbar', title: 'Hide Toolbar', shortcut: '⌥⌘T' },
+          { id: 'hide-path-bar', title: 'Hide Path Bar', shortcut: '⌥⌘P' },
+          { id: 'hide-status-bar', title: 'Hide Status Bar', shortcut: '⌘/' },
+          { id: 'customise-toolbar', title: 'Customise Toolbar...' },
           {
-            id: 'general',
-            title: 'General',
-            action: () => console.log('General Preferences')
+            id: 'show-view-options',
+            title: 'Show View Options',
+            shortcut: '⌘J'
           },
           {
-            id: 'appearance',
-            title: 'Appearance',
-            action: () => console.log('Appearance Preferences')
+            id: 'enter-full-screen',
+            title: 'Enter Full Screen',
+            shortcut: '⌃⌘F'
           }
         ]
       },
@@ -141,15 +163,32 @@ export const apps: AppData[] = [
         id: 'go',
         label: 'Go',
         menu: [
+          { id: 'back', title: 'Back', shortcut: '⌃Ö' },
+          { id: 'forward', title: 'Forward', shortcut: '⌃Ä' },
           {
-            id: 'general',
-            title: 'General',
-            action: () => console.log('General Preferences')
+            id: 'select-startup-disk',
+            title: 'Select Startup Disk',
+            shortcut: '⇧⌃⌘A'
           },
+          { id: 'recents', title: 'Recents', shortcut: '⇧⌘F' },
+          { id: 'documents', title: 'Documents', shortcut: '⇧⌘O' },
+          { id: 'desktop', title: 'Desktop', shortcut: '⇧⌘D' },
+          { id: 'downloads', title: 'Downloads', shortcut: '⌥⌘L' },
+          { id: 'home', title: 'Home', shortcut: '⇧⌘H' },
+          { id: 'library', title: 'Library', shortcut: '⇧⌘L' },
+          { id: 'computer', title: 'Computer', shortcut: '⇧⌘C' },
+          { id: 'airdrop', title: 'AirDrop', shortcut: '⇧⌘R' },
+          { id: 'network', title: 'Network', shortcut: '⇧⌘K' },
+          { id: 'icloud-drive', title: 'iCloud Drive', shortcut: '⇧⌘I' },
+          { id: 'shared', title: 'Shared', shortcut: '⇧⌘S' },
+          { id: 'applications', title: 'Applications', shortcut: '⇧⌘A' },
+          { id: 'utilities', title: 'Utilities', shortcut: '⇧⌘U' },
+          { id: 'recent-folders', title: 'Recent Folders', subMenu: [] },
+          { id: 'go-to-folder', title: 'Go to Folder...', shortcut: '⌘G' },
           {
-            id: 'appearance',
-            title: 'Appearance',
-            action: () => console.log('Appearance Preferences')
+            id: 'connect-to-server',
+            title: 'Connect to Server...',
+            shortcut: '⌘K'
           }
         ]
       },
@@ -157,32 +196,40 @@ export const apps: AppData[] = [
         id: 'window',
         label: 'Window',
         menu: [
+          { id: 'minimise', title: 'Minimise', shortcut: '⌘M' },
+          { id: 'zoom', title: 'Zoom' },
           {
-            id: 'general',
-            title: 'General',
-            action: () => console.log('General Preferences')
+            id: 'tile-window-to-left-of-screen',
+            title: 'Tile Window to Left of Screen'
           },
           {
-            id: 'appearance',
-            title: 'Appearance',
-            action: () => console.log('Appearance Preferences')
-          }
+            id: 'tile-window-to-right-of-screen',
+            title: 'Tile Window to Right of Screen'
+          },
+          {
+            id: 'cycle-through-windows',
+            title: 'Cycle Through Windows',
+            shortcut: '⌘<'
+          },
+          {
+            id: 'show-previous-tab',
+            title: 'Show Previous Tab',
+            shortcut: '⇧⌃⇥'
+          },
+          { id: 'show-next-tab', title: 'Show Next Tab', shortcut: '⇧⌃⇧⇥' },
+          { id: 'move-tab-to-new-window', title: 'Move Tab to New Window' },
+          { id: 'merge-all-windows', title: 'Merge All Windows' },
+          { id: 'bring-all-to-front', title: 'Bring All to Front' },
+          { id: 'recents', title: 'Recents' }
         ]
       },
       {
         id: 'help',
         label: 'Help',
         menu: [
-          {
-            id: 'general',
-            title: 'General',
-            action: () => console.log('General Preferences')
-          },
-          {
-            id: 'appearance',
-            title: 'Appearance',
-            action: () => console.log('Appearance Preferences')
-          }
+          { id: 'search', title: 'Search' },
+          { id: 'tips-for-your-mac', title: 'Tips for Your Mac' },
+          { id: 'macos-help', title: 'macOS Help' }
         ]
       }
     ]
@@ -196,46 +243,7 @@ export const apps: AppData[] = [
     z: 0,
     size: { width: 640, height: 400 },
     position: { x: 0, y: 0 },
-    triggers: [
-      {
-        id: 'title',
-        label: 'Finder',
-        menu: [
-          {
-            id: 'about',
-            title: 'About Finder',
-            action: () => console.log('About Finder')
-          },
-          {
-            id: 'settings',
-            title: 'Settings...',
-            action: () => console.log('Open Settings')
-          },
-          {
-            id: 'quit',
-            title: 'Quit Finder',
-            shortcut: '⌘Q',
-            action: () => console.log('Quit Finder')
-          }
-        ]
-      },
-      {
-        id: 'preferences',
-        label: 'Preferences',
-        menu: [
-          {
-            id: 'general',
-            title: 'General',
-            action: () => console.log('General Preferences')
-          },
-          {
-            id: 'appearance',
-            title: 'Appearance',
-            action: () => console.log('Appearance Preferences')
-          }
-        ]
-      }
-    ]
+    triggers: []
   },
   {
     id: 'arc',
