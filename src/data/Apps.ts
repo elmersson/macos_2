459102051455bb05af174c34d7a847ddb8fd68ec
elmersson <elmerssonrasmus@gmail.com>
2024-06-21 07:52:43 +0200

    feat: menu data for arc (#85)

--- src/data/Apps.ts
+++ src/data/Apps.ts
@@ -257,41 +257,271 @@ export const apps: AppData[] = [
     position: { x: 0, y: 0 },
     triggers: [
       {
-        id: 'title',
-        label: 'Finder',
+        id: 'arc',
+        label: 'Arc',
         menu: [
+          { id: 'about-arc', title: 'About Arc' },
+          { id: 'settings', title: 'Settings...', shortcut: '⌘,' },
+          { id: 'share-arc', title: 'Share Arc' },
           {
-            id: 'about',
-            title: 'About Finder',
-            action: () => console.log('About Finder')
+            id: 'set-as-default-browser',
+            title: 'Set as Default Browser',
+            shortcut: '⌘♥'
           },
           {
-            id: 'settings',
-            title: 'Settings...',
-            action: () => console.log('Open Settings')
+            id: 'import-from-another-browser',
+            title: 'Import from Another Browser...'
           },
           {
-            id: 'quit',
-            title: 'Quit Finder',
-            shortcut: '⌘Q',
-            action: () => console.log('Quit Finder')
+            id: 'update-available',
+            title: 'Update Available...',
+            subMenu: [{ id: 'update', title: '1 update' }]
+          },
+          { id: 'services', title: 'Services', subMenu: [] },
+          { id: 'privacy-policy', title: 'Privacy Policy' },
+          { id: 'hide-arc', title: 'Hide Arc', shortcut: '⌘H' },
+          {
+            id: 'hide-other-windows',
+            title: 'Hide Other Windows',
+            shortcut: '⌥⌘H'
+          },
+          { id: 'show-all', title: 'Show All' },
+          { id: 'sign-out', title: 'Sign Out' },
+          { id: 'quit-arc', title: 'Quit Arc', shortcut: '⌘Q' }
+        ]
+      },
+      {
+        id: 'file',
+        label: 'File',
+        menu: [
+          { id: 'new-tab', title: 'New Tab...', shortcut: '⌘T' },
+          { id: 'new-window', title: 'New Window', shortcut: '⌘N' },
+          { id: 'blank-window', title: 'Blank Window', shortcut: '⇧⌘N' },
+          {
+            id: 'new-incognito-window',
+            title: 'New Incognito Window',
+            shortcut: '⌥⌘N'
+          },
+          {
+            id: 'new-little-arc-window',
+            title: 'New Little Arc Window',
+            shortcut: '⌥⌘N'
+          },
+          {
+            id: 'restore-last-closed-tab',
+            title: 'Restore Last Closed Tab',
+            shortcut: '⇧⌘T'
+          },
+          { id: 'open-command-bar', title: 'Open Command Bar', shortcut: '⌘L' },
+          { id: 'new-easel', title: 'New Easel', shortcut: '⌃⌘E' },
+          { id: 'close-window', title: 'Close Window', shortcut: '⇧⌘W' },
+          { id: 'archive-tab', title: 'Archive Tab', shortcut: '⌘W' },
+          { id: 'share', title: 'Share...', subMenu: [] },
+          { id: 'capture', title: 'Capture...', shortcut: '⌃⌘2' },
+          { id: 'capture-full-page', title: 'Capture Full Page' },
+          { id: 'capture-portrait-mode', title: 'Capture in Portrait Mode' },
+          { id: 'save-page-as', title: 'Save Page As', shortcut: '⌃⇧⌘S' },
+          { id: 'print', title: 'Print', shortcut: '⌘P' }
+        ]
+      },
+      {
+        id: 'edit',
+        label: 'Edit',
+        menu: [
+          { id: 'undo', title: 'Undo', shortcut: '⌘Z' },
+          { id: 'redo', title: 'Redo', shortcut: '⇧⌘Z' },
+          { id: 'cut', title: 'Cut', shortcut: '⌘X' },
+          { id: 'copy', title: 'Copy', shortcut: '⌘C' },
+          { id: 'copy-url', title: 'Copy URL', shortcut: '⌥⌘C' },
+          {
+            id: 'copy-url-as-markdown',
+            title: 'Copy URL as Markdown',
+            shortcut: '⌃⇧⌘C'
+          },
+          {
+            id: 'copy-url-as-quote',
+            title: 'Copy URL as Quote',
+            shortcut: '⌃⌘C'
+          },
+          { id: 'paste', title: 'Paste', shortcut: '⌘V' },
+          {
+            id: 'paste-and-match-style',
+            title: 'Paste and Match Style',
+            shortcut: '⌥⇧⌘V'
+          },
+          { id: 'delete', title: 'Delete' },
+          { id: 'select-all', title: 'Select All', shortcut: '⌘A' },
+          { id: 'find', title: 'Find', subMenu: [] },
+          {
+            id: 'spelling-and-grammar',
+            title: 'Spelling and Grammar',
+            subMenu: []
+          },
+          { id: 'substitutions', title: 'Substitutions', subMenu: [] },
+          { id: 'transformations', title: 'Transformations', subMenu: [] },
+          { id: 'speech', title: 'Speech', subMenu: [] },
+          { id: 'format', title: 'Format', subMenu: [] },
+          { id: 'autofill', title: 'AutoFill', subMenu: [] },
+          { id: 'start-dictation', title: 'Start Dictation...' },
+          { id: 'emoji-symbols', title: 'Emoji & Symbols' }
+        ]
+      },
+      {
+        id: 'view',
+        label: 'View',
+        menu: [
+          { id: 'appearance', title: 'Appearance', subMenu: [] },
+          { id: 'hide-sidebar', title: 'Hide Sidebar', shortcut: '⌘S' },
+          { id: 'show-toolbar', title: 'Show Toolbar', shortcut: '⇧⌘D' },
+          { id: 'collapse-pinned-tabs', title: 'Collapse Pinned Tabs' },
+          { id: 'stop-loading', title: 'Stop Loading', shortcut: '⌘.' },
+          {
+            id: 'force-refresh',
+            title: 'Force Refresh the Page',
+            shortcut: '⌘⇧R'
+          },
+          { id: 'clear-cookies-refresh', title: 'Clear Cookies and Refresh' },
+          { id: 'clear-cache-refresh', title: 'Clear Cache and Refresh' },
+          { id: 'add-split-view', title: 'Add Split View', shortcut: '⌃⌘+' },
+          {
+            id: 'close-split-pane',
+            title: 'Close this Split Pane',
+            shortcut: '⌃⌘-'
+          },
+          {
+            id: 'separate-page-split-view',
+            title: 'Separate Page from Split View'
+          },
+          { id: 'expand-current-split', title: 'Expand Current Split' },
+          {
+            id: 'zoom-actual-size',
+            title: 'Zoom to Actual Size',
+            shortcut: '⌘0'
+          },
+          { id: 'zoom-in', title: 'Zoom In', shortcut: '⌘+' },
+          { id: 'zoom-out', title: 'Zoom Out', shortcut: '⌘-' },
+          { id: 'cast', title: 'Cast' },
+          { id: 'developer', title: 'Developer', subMenu: [] },
+          {
+            id: 'enter-exit-reader-mode',
+            title: 'Enter/Exit Reader Mode (beta)',
+            shortcut: '⌥⌘R'
+          },
+          {
+            id: 'enter-full-screen',
+            title: 'Enter Full Screen',
+            shortcut: '⌃⌘F'
           }
         ]
       },
       {
-        id: 'preferences',
-        label: 'Preferences',
+        id: 'spaces',
+        label: 'Spaces',
         menu: [
+          { id: 'new-space', title: 'New Space...' },
+          { id: 'edit-theme', title: 'Edit Theme...' },
+          { id: 'rename-space', title: 'Rename Space' },
+          { id: 'next-space', title: 'Next Space', shortcut: '⌃⌘→' },
+          { id: 'previous-space', title: 'Previous Space', shortcut: '⌃⌘←' },
+          { id: '🤓 private-space', title: 'Privat', shortcut: '⌃1' },
+          { id: '👨‍💻 work-space', title: 'Work', shortcut: '⌃2' },
+          { id: 'manage-spaces', title: 'Manage Spaces...' }
+        ]
+      },
+      {
+        id: 'tabs',
+        label: 'Tabs',
+        menu: [
+          { id: 'new-tab', title: 'New Tab...', shortcut: '⌘T' },
+          { id: 'pin-tab', title: 'Pin Tab', shortcut: '⌘D' },
+          { id: 'new-folder', title: 'New Folder...' },
+          { id: 'next-tab', title: 'Next Tab', shortcut: '⌃⇧⌘▼' },
+          { id: 'previous-tab', title: 'Previous Tab', shortcut: '⌃⇧⌘▲' },
+          { id: 'reveal-tab', title: 'Reveal Tab in Sidebar' },
+          { id: 'clear-today', title: 'Clear Today', shortcut: '⌃⇧⌘K' },
+          { id: 'reset-tabs', title: 'Reset all tabs in this Space' }
+        ]
+      },
+      {
+        id: 'archive',
+        label: 'Archive',
+        menu: [
+          { id: 'go-back', title: 'Go Back', shortcut: '⌘Ö' },
+          { id: 'go-forward', title: 'Go Forward', shortcut: '⌘Ä' },
+          { id: 'view-history', title: 'View History', shortcut: '⌘Y' },
+          { id: 'view-archive', title: 'View Archive...' },
+          { id: 'clear-archive', title: 'Clear Archive' }
+        ]
+      },
+      {
+        id: 'extensions',
+        label: 'Extensions',
+        menu: [
+          { id: '1password', title: '1Password – Password Manager' },
           {
-            id: 'general',
-            title: 'General',
-            action: () => console.log('General Preferences')
+            id: 'adobe-acrobat',
+            title: 'Adobe Acrobat: PDF edit, convert, sign tools'
           },
           {
-            id: 'appearance',
-            title: 'Appearance',
-            action: () => console.log('Appearance Preferences')
-          }
+            id: 'browsing-protection',
+            title: 'Browsing Protection by F-Secure'
+          },
+          { id: 'fontanello', title: 'Fontanello' },
+          { id: 'google-docs-offline', title: 'Google Docs Offline' },
+          { id: 'json-formatter', title: 'JSON Formatter' },
+          {
+            id: 'le-git-graph',
+            title: 'Le Git Graph - Commits Graph for GitHub'
+          },
+          { id: 'metamask', title: 'MetaMask' },
+          { id: 'phantom', title: 'Phantom' },
+          {
+            id: 'picture-in-picture',
+            title: 'Picture-in-Picture Extension (by Google)'
+          },
+          { id: 'redux-devtools', title: 'Redux DevTools' },
+          { id: 'ublock-origin', title: 'uBlock Origin' },
+          { id: 'add-extension', title: 'Add Extension...' },
+          { id: 'manage-extensions', title: 'Manage Extensions...' }
+        ]
+      },
+
+      {
+        id: 'window',
+        label: 'Window',
+        menu: [
+          { id: 'stay-on-top', title: 'Stay On Top' },
+          { id: 'minimize-window', title: 'Minimize Window', shortcut: '⌘M' },
+          { id: 'zoom', title: 'Zoom' },
+          { id: 'tile-window-left', title: 'Tile Window to Left of Screen' },
+          { id: 'tile-window-right', title: 'Tile Window to Right of Screen' },
+          { id: 'replace-tiled-window', title: 'Replace Tiled Window' },
+          { id: 'remove-window-from-set', title: 'Remove Window from Set' },
+          { id: 'open-library', title: 'Open Library...', shortcut: '⌃⌘L' },
+          { id: 'view-downloads', title: 'View Downloads...', shortcut: '⌃⌘J' },
+          { id: 'view-easels', title: 'View Easels...' },
+          { id: 'view-media', title: 'View Media...' },
+          { id: 'view-boosts', title: 'View Boosts...' },
+          { id: 'bring-all-to-front', title: 'Bring All to Front' },
+          { id: 'localhost-3000', title: 'localhost:3000' },
+          { id: 'rasmus-elmersson', title: 'Rasmus Elmersson', shortcut: '✓' }
+        ]
+      },
+      {
+        id: 'help',
+        label: 'Help',
+        menu: [
+          { id: 'search', title: 'Search' },
+          { id: 'getting-started', title: 'Getting Started' },
+          {
+            id: 'essential-keyboard-shortcuts',
+            title: 'Essential Keyboard Shortcuts'
+          },
+          { id: 'contact-the-team', title: 'Contact the Team' },
+          { id: 'visit-help-center', title: 'Visit Help Center' },
+          { id: 'restore-data', title: 'Restore Data' },
+          { id: 'export-arc-notes', title: 'Export Arc Notes' },
+          { id: 'troubleshooting', title: 'Troubleshooting', subMenu: [] }
         ]
       }
     ]
