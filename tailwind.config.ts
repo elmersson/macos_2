7d3b824a3d8576c31151ca878ac08b1658d1e035
elmersson <elmerssonrasmus@gmail.com>
2024-07-19 09:17:55 +0200

    feat: system design displays (#112)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,8 @@ const config = {
         'apple-blue': '#167AE5'
       },
       fontSize: {
-        '3xs': ['0.5rem', '0.0rem'],
+        '4xs': ['0.3rem', '0.4rem'],
+        '3xs': ['0.5rem', '0.6rem'],
         xxs: ['0.6rem', '0.7rem'],
         xs: ['0.688rem', '0.9rem'],
         sm: ['0.813rem', '1.2rem']
