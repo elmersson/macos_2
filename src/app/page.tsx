e9d86a24419c0f98e727d0c3ef7d2831003fa90c
RasmusElmersson <126062642+RasmusElmersson@users.noreply.github.com>
2024-09-10 19:01:21 +0200

    feat: sleep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,29 @@ import { Login } from '@/components/login';
 import { Navbar } from '@/components/navbar';
 import { useSystemStore } from '@/components/providers/store-provider';
 import Wallpaper from '@/components/wallpaper';
+import { useEffect } from 'react';
 
 export default function Home() {
-  const { logedIn, display, launchPad } = useSystemStore((state) => state);
+  const { logedIn, display, launchPad, setDisplay, isSleeping, setIsSleeping } =
+    useSystemStore((state) => state);
   const opacity = display * 0.01;
+
+  useEffect(() => {
+    const handleWakeUp = () => {
+      if (isSleeping) {
+        setDisplay(100);
+        setIsSleeping(false);
+      }
+    };
+
+    window.addEventListener('mousemove', handleWakeUp);
+    window.addEventListener('keypress', handleWakeUp);
+
+    return () => {
+      window.removeEventListener('mousemove', handleWakeUp);
+      window.removeEventListener('keypress', handleWakeUp);
+    };
+  }, [isSleeping, setDisplay, setIsSleeping]);
 
   return (
     <div style={{ opacity }}>
