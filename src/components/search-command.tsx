4406ec74ea32f09e61c9a184f282997071f82824
RasmusElmersson <126062642+RasmusElmersson@users.noreply.github.com>
2024-09-18 18:03:50 +0200

    fix: design raycast

--- src/components/search-command.tsx
+++ src/components/search-command.tsx
@@ -11,9 +11,14 @@ import {
 } from './ui/command';
 import { CommandEmpty } from 'cmdk';
 import Image from 'next/image';
+import { SiRaycast } from 'react-icons/si';
+import { cn } from '@/lib/utils';
+import { AiOutlineEnter } from 'react-icons/ai';
 
 export function SearchCommand() {
   const [isMounted, setIsMounted] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
   const { openApp, bringToFront, apps } = useAppStore((state) => state);
   const { isOpen, toggle, onClose } = useSystemStore((state) => state);
 
@@ -45,23 +50,48 @@ export function SearchCommand() {
 
   return (
     <CommandDialog open={isOpen} onOpenChange={onClose}>
-      <CommandInput placeholder="Search for apps" />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Welcome to Raycast">
-          {apps.map((app) => (
-            <CommandItem
-              key={app.id}
-              title={app.title}
-              value={`${app.id}-${app.title}`}
-              onSelect={() => onSelect(app.id)}
-            >
-              <Image src={app.img} alt={app.title} className="w-6" />
-              <span className="ml-4">{app.title}</span>
-            </CommandItem>
-          ))}
-        </CommandGroup>
-      </CommandList>
+      <CommandInput
+        placeholder="Search for apps"
+        value={inputValue}
+        onValueChange={setInputValue}
+      />
+      {inputValue.length > 0 && (
+        <CommandList className="scrollbar-hide">
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Welcome to Raycast">
+            {apps.map((app) => (
+              <CommandItem
+                key={app.id}
+                title={app.title}
+                value={`${app.id}-${app.title}`}
+                onSelect={() => onSelect(app.id)}
+              >
+                <Image src={app.img} alt={app.title} className="w-6" />
+                <span className="ml-4">{app.title}</span>
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      )}
+      <div className="flex items-center justify-between border-t border-gray-700 py-3">
+        <div className="flex items-center space-x-2 text-sm text-gray-400">
+          <SiRaycast
+            size={20}
+            className={cn(
+              'text-orange-600 ml-4 mr-2',
+              inputValue.length > 0 && 'text-neutral-500'
+            )}
+          />
+          {inputValue === '' && <span>See what&apos;s new? v1.83.0</span>}
+        </div>
+
+        <kbd className="flex items-center space-x-4 mr-4">
+          <span className="text-sm font-bold">Open App</span>
+          <span className="bg-neutral-400/20 rounded-sm p-1">
+            <AiOutlineEnter />
+          </span>
+        </kbd>
+      </div>
     </CommandDialog>
   );
 }
