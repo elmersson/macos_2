0488fa32610ce66e17f47c4b893c4e4e1215590e
RasmusElmersson <126062642+RasmusElmersson@users.noreply.github.com>
2024-09-21 17:06:25 +0300

    feat: math in search

--- src/components/search-command.tsx
+++ src/components/search-command.tsx
@@ -14,10 +14,12 @@ import Image from 'next/image';
 import { SiRaycast } from 'react-icons/si';
 import { cn } from '@/lib/utils';
 import { AiOutlineEnter } from 'react-icons/ai';
+import { evaluate } from 'mathjs';
 
 export function SearchCommand() {
   const [isMounted, setIsMounted] = useState(false);
   const [inputValue, setInputValue] = useState('');
+  const [mathResult, setMathResult] = useState<string | null>(null);
 
   const { openApp, bringToFront, apps } = useAppStore((state) => state);
   const { isOpen, toggle, onClose } = useSystemStore((state) => state);
@@ -44,9 +46,35 @@ export function SearchCommand() {
     onClose();
   };
 
+  useEffect(() => {
+    const evaluateMath = (input: string) => {
+      const mathRegex = /^[0-9+\-*/().\s]+$/;
+
+      console.log(mathRegex.test(input));
+      if (mathRegex.test(input)) {
+        try {
+          const result = evaluate(input);
+
+          console.log(result);
+          setMathResult(result.toString());
+        } catch {
+          setMathResult('Error');
+        }
+      } else {
+        setMathResult(null);
+      }
+    };
+
+    evaluateMath(inputValue);
+  }, [inputValue]);
+
   if (!isMounted) {
     return null;
   }
+
+  const appList = Array.isArray(apps) ? apps : [];
+
+  console.log(appList);
 
   return (
     <CommandDialog open={isOpen} onOpenChange={onClose}>
@@ -55,23 +83,33 @@ export function SearchCommand() {
         value={inputValue}
         onValueChange={setInputValue}
       />
-      {inputValue.length > 0 && (
+      {inputValue.length > 0 && mathResult !== null ? (
         <CommandList className="scrollbar-hide">
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Welcome to Raycast">
-            {apps.map((app) => (
-              <CommandItem
-                key={app.id}
-                title={app.title}
-                value={`${app.id}-${app.title}`}
-                onSelect={() => onSelect(app.id)}
-              >
-                <Image src={app.img} alt={app.title} className="w-6" />
-                <span className="ml-4">{app.title}</span>
-              </CommandItem>
-            ))}
-          </CommandGroup>
+          <CommandItem>
+            <span className="ml-4">Result: {mathResult}</span>
+          </CommandItem>
         </CommandList>
+      ) : (
+        inputValue.length > 0 && (
+          <CommandList className="scrollbar-hide">
+            <CommandEmpty>No results found.</CommandEmpty>
+            {appList && appList.length > 0 && (
+              <CommandGroup heading="Welcome to Raycast">
+                {appList.map((app) => (
+                  <CommandItem
+                    key={app.id}
+                    title={app.title}
+                    value={`${app.id}-${app.title}`}
+                    onSelect={() => onSelect(app.id)}
+                  >
+                    <Image src={app.img} alt={app.title} className="w-6" />
+                    <span className="ml-4">{app.title}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            )}
+          </CommandList>
+        )
       )}
       <div className="flex items-center justify-between border-t border-gray-700 py-3">
         <div className="flex items-center space-x-2 text-sm text-gray-400">
